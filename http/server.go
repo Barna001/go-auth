15f82a57cf421925ff452ecde6c54172f9399263
Mark Mikitovics <mikitovicsm@gmail.com>
2018-03-13 11:32:29 +0100

    fix(server): handle no email query

--- http/server.go
+++ http/server.go
@@ -36,16 +36,24 @@ func (server Server) handleUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleGet(w http.ResponseWriter, r *http.Request, db database.Database) {
-	email := r.URL.Query()["email"][0]
-	user, err := db.GetUser(email)
+	email := r.URL.Query()["email"]
+	if len(email) == 0 {
+		http.Error(w, "You have to give an email address", http.StatusBadRequest)
+		return
+	}
+
+	user, err := db.GetUser(email[0])
 	if err != nil {
 		switch err.(type) {
 		case errors.NoUserError:
 			http.Error(w, "No user with this email", http.StatusNotFound)
+			return
 		default:
 			http.Error(w, "Can not read from DB", http.StatusInternalServerError)
+			return
 		}
 	}
 	userJSON, _ := json.Marshal(user)
 	fmt.Fprintf(w, string(userJSON))
+	return
 }
